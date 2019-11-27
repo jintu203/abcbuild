ae1f1c11961f9015c477101be4c373c20b780304
jintu <jintusabu@gmail.com>
2019-11-27 17:17:42 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
